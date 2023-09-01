851bfa1f1c9b659ca49ebab8301da5d0bd078e9d
Itzdlg <dontyoutrybrah@gmail.com>
2023-09-01 10:34:08 -0400

    Add session logic for password-based accounts

--- backend/src/main/sh/dominick/hoojtracker/modules/accounts/AccountsModule.kt
+++ backend/src/main/sh/dominick/hoojtracker/modules/accounts/AccountsModule.kt
@@ -1,13 +1,18 @@
 package sh.dominick.hoojtracker.modules.accounts
 
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
 import io.javalin.community.routing.annotations.AnnotatedRoutingPlugin
 import io.javalin.config.JavalinConfig
+import org.jetbrains.exposed.sql.lowerCase
+import org.jetbrains.exposed.sql.transactions.transaction
 import sh.dominick.hoojtracker.auth.Authorization
-import sh.dominick.hoojtracker.modules.accounts.data.AccountCredentialsTable
-import sh.dominick.hoojtracker.modules.accounts.data.AccountsTable
 import sh.dominick.hoojtracker.modules.Module
+import sh.dominick.hoojtracker.modules.accounts.data.*
 import sh.dominick.hoojtracker.modules.accounts.routes.AccountsController
+import sh.dominick.hoojtracker.modules.sessions.SessionsModule
+import sh.dominick.hoojtracker.modules.sessions.data.Session
+import java.time.Instant
 
 object AccountsModule : Module("accounts") {
     override val tables = setOf(
@@ -24,6 +29,32 @@ object AccountsModule : Module("accounts") {
 
         Authorization.registerType("Basic") {
             BasicAuth.match(it)
+        }
+
+        SessionsModule.registerType("password") {
+            val email = it.get("email")?.asString
+                ?: throw IllegalArgumentException("The provided body is missing an email.")
+
+            val password = it.get("password")?.asString
+                ?: throw IllegalArgumentException("The provided body is missing a password.")
+
+            transaction {
+                val account = Account.find {
+                    AccountsTable.email.lowerCase() eq email.lowercase()
+                }.firstOrNull() ?: throw IllegalArgumentException("No account with that email was found.")
+
+                if (!account.isPassword(password))
+                    throw IllegalArgumentException("The provided password is not correct.")
+
+                val session = Session.new {
+                    this.account = account
+                    this.createdAt = Instant.now()
+                    this.expiresAt = Instant.now()
+                    this.metadata = JsonObject()
+                }
+
+                return@transaction session
+            }
         }
     }
 }

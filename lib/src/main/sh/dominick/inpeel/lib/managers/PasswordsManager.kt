Itzdlg/Hooj-Tracker

commit 9d6094bed8999fbc9e964c75ce5f5fbf258c0c06

Abstract from rest-backend to lib.

lib/pom.xml
@@ -1,0 +1,253 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+    <modelVersion>4.0.0</modelVersion>
+    <parent>
+        <groupId>sh.dominick.inpeel</groupId>
+        <artifactId>inpeel</artifactId>
+        <version>1.0.0</version>
+    </parent>
+
+    <artifactId>lib</artifactId>
+
+    <properties>
+        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+        <kotlin.code.style>official</kotlin.code.style>
+        <kotlin.compiler.jvmTarget>1.8</kotlin.compiler.jvmTarget>
+    </properties>
+
+    <repositories>
+        <repository>
+            <id>mavenCentral</id>
+            <url>https://repo1.maven.org/maven2/</url>
+        </repository>
+    </repositories>
+
+    <build>
+        <sourceDirectory>src/main</sourceDirectory>
+        <testSourceDirectory>src/test</testSourceDirectory>
+
+        <plugins>
+            <plugin>
+                <groupId>org.apache.maven.plugins</groupId>
+                <artifactId>maven-resources-plugin</artifactId>
+                <version>3.3.1</version>
+
+                <configuration>
+                    <testResources>
+                        <testResource>
+                            <directory>${project.basedir}/src/test</directory>
+                        </testResource>
+                    </testResources>
+                </configuration>
+            </plugin>
+            <plugin>
+                <groupId>org.jetbrains.kotlin</groupId>
+                <artifactId>kotlin-maven-plugin</artifactId>
+                <version>1.7.20</version>
+                <configuration>
+                    <javaParameters>true</javaParameters>
+                </configuration>
+                <executions>
+                    <execution>
+                        <id>compile</id>
+                        <phase>compile</phase>
+                        <goals>
+                            <goal>compile</goal>
+                        </goals>
+                    </execution>
+                    <execution>
+                        <id>test-compile</id>
+                        <phase>test-compile</phase>
+                        <goals>
+                            <goal>test-compile</goal>
+                        </goals>
+                    </execution>
+                </executions>
+            </plugin>
+            <plugin>
+                <artifactId>maven-surefire-plugin</artifactId>
+                <version>2.22.2</version>
+            </plugin>
+            <plugin>
+                <artifactId>maven-failsafe-plugin</artifactId>
+                <version>2.22.2</version>
+            </plugin>
+            <plugin>
+                <groupId>org.apache.maven.plugins</groupId>
+                <artifactId>maven-compiler-plugin</artifactId>
+                <configuration>
+                    <source>11</source>
+                    <target>11</target>
+                    <compilerArgs>
+                        <arg>-parameters</arg>
+                    </compilerArgs>
+                </configuration>
+            </plugin>
+            <plugin>
+                <groupId>org.apache.maven.plugins</groupId>
+                <artifactId>maven-jar-plugin</artifactId>
+                <version>2.6</version>
+                <configuration>
+                    <archive>
+                        <manifest>
+                            <mainClass>sh.dominick.inpeel.ApplicationKt</mainClass>
+                        </manifest>
+                    </archive>
+                </configuration>
+            </plugin>
+        </plugins>
+    </build>
+
+    <dependencies>
+        <dependency>
+            <groupId>org.jetbrains.kotlin</groupId>
+            <artifactId>kotlin-stdlib-jdk8</artifactId>
+            <version>1.7.20</version>
+        </dependency>
+
+        <!-- https://mvnrepository.com/artifact/io.javalin/javalin -->
+        <dependency>
+            <groupId>io.javalin</groupId>
+            <artifactId>javalin</artifactId>
+            <version>5.3.2</version>
+        </dependency>
+
+        <dependency>
+            <groupId>org.slf4j</groupId>
+            <artifactId>slf4j-simple</artifactId>
+            <version>2.0.3</version>
+        </dependency>
+
+        <dependency>
+            <groupId>org.jetbrains.exposed</groupId>
+            <artifactId>exposed-core</artifactId>
+            <version>0.40.1</version>
+        </dependency>
+        <dependency>
+            <groupId>org.jetbrains.exposed</groupId>
+            <artifactId>exposed-dao</artifactId>
+            <version>0.40.1</version>
+        </dependency>
+        <dependency>
+            <groupId>org.jetbrains.exposed</groupId>
+            <artifactId>exposed-jdbc</artifactId>
+            <version>0.40.1</version>
+        </dependency>
+
+        <dependency>
+            <groupId>redis.clients</groupId>
+            <artifactId>jedis</artifactId>
+            <version>4.3.0</version>
+        </dependency>
+
+        <dependency>
+            <groupId>io.github.cdimascio</groupId>
+            <artifactId>dotenv-kotlin</artifactId>
+            <version>6.4.1</version>
+        </dependency>
+
+        <dependency>
+            <groupId>io.jsonwebtoken</groupId>
+            <artifactId>jjwt-api</artifactId>
+            <version>0.11.5</version>
+        </dependency>
+        <dependency>
+            <groupId>io.jsonwebtoken</groupId>
+            <artifactId>jjwt-impl</artifactId>
+            <version>0.11.5</version>
+            <scope>runtime</scope>
+        </dependency>
+        <dependency>
+            <groupId>io.jsonwebtoken</groupId>
+            <artifactId>jjwt-jackson</artifactId>
+            <version>0.11.5</version>
+            <scope>runtime</scope>
+        </dependency>
+
+        <dependency>
+            <groupId>mysql</groupId>
+            <artifactId>mysql-connector-java</artifactId>
+            <version>8.0.32</version>
+        </dependency>
+
+        <dependency>
+            <groupId>com.zaxxer</groupId>
+            <artifactId>HikariCP</artifactId>
+            <version>4.0.3</version>
+        </dependency>
+
+        <dependency>
+            <groupId>io.javalin.community.routing</groupId>
+            <artifactId>routing-core</artifactId>
+            <version>5.5.0-RC.3</version>
+        </dependency>
+
+        <dependency>
+            <groupId>io.javalin.community.routing</groupId>
+            <artifactId>routing-annotated</artifactId>
+            <version>5.5.0-RC.3</version>
+        </dependency>
+
+        <dependency>
+            <groupId>io.javalin.community.routing</groupId>
+            <artifactId>routing-coroutines</artifactId>
+            <version>5.5.0-RC.3</version>
+        </dependency>
+
+        <!-- Argon2 Hashing Implementation -->
+        <!--  - Account Passwords -->
+        <dependency>
+            <groupId>de.mkammerer</groupId>
+            <artifactId>argon2-jvm</artifactId>
+            <version>2.7</version>
+        </dependency>
+
+        <!-- OAuth2 -->
+        <dependency>
+            <groupId>com.google.oauth-client</groupId>
+            <artifactId>google-oauth-client</artifactId>
+            <version>1.33.3</version>
+        </dependency>
+
+        <!-- Testing Dependencies -->
+        <dependency>
+            <groupId>junit</groupId>
+            <artifactId>junit</artifactId>
+            <version>4.13.1</version>
+            <scope>test</scope>
+        </dependency>
+
+        <dependency>
+            <groupId>org.assertj</groupId>
+            <artifactId>assertj-core</artifactId>
+            <version>3.11.1</version>
+            <scope>test</scope>
+        </dependency>
+
+        <dependency>
+            <groupId>io.mockk</groupId>
+            <artifactId>mockk</artifactId>
+            <version>1.12.5</version>
+            <scope>test</scope>
+        </dependency>
+
+        <dependency>
+            <groupId>org.xerial</groupId>
+            <artifactId>sqlite-jdbc</artifactId>
+            <version>3.42.0.0</version>
+            <scope>test</scope>
+        </dependency>
+
+        <dependency>
+            <groupId>com.h2database</groupId>
+            <artifactId>h2</artifactId>
+            <version>2.2.220</version>
+            <scope>test</scope>
+        </dependency>
+
+
+    </dependencies>
+
+</project>

rest-backend/src/main/sh/dominick/inpeel/rest/Application.kt
@@ -1,0 +1,86 @@
+package sh.dominick.inpeel.rest
+
+import com.google.gson.GsonBuilder
+import com.google.gson.LongSerializationPolicy
+import com.zaxxer.hikari.HikariDataSource
+import io.javalin.Javalin
+import io.javalin.community.routing.annotations.AnnotatedRoutingPlugin
+import io.javalin.json.JsonMapper
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.DatabaseConfig
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+import sh.dominick.inpeel.lib.data.oauth2.sql.OAuth2ConnectionsTable
+import sh.dominick.inpeel.lib.data.passwords.sql.AccountPasswordsTable
+import sh.dominick.inpeel.lib.data.sessions.sql.SessionsTable
+import sh.dominick.inpeel.lib.data.sql.AccountsTable
+import sh.dominick.inpeel.lib.data.sql.Configuration
+import sh.dominick.inpeel.lib.data.sql.ConfigurationTable
+import sh.dominick.inpeel.lib.managers.OAuth2ConnectionsManager
+import sh.dominick.inpeel.lib.managers.SessionManager
+import java.lang.reflect.Type
+
+var gson = GsonBuilder()
+    .disableHtmlEscaping()
+    .setLongSerializationPolicy(LongSerializationPolicy.STRING)
+    .create()
+private set
+
+var prettyGson = GsonBuilder().create()
+private set
+
+fun main() {
+    val dataSource = HikariDataSource().apply {
+        maximumPoolSize = Env.DATABASE_POOL_SIZE
+        driverClassName = Env.DATABASE_DRIVER
+        jdbcUrl = Env.DATABASE_URL
+        username = Env.DATABASE_USERNAME
+        password = Env.DATABASE_PASSWORD
+        isAutoCommit = false
+    }
+
+    val database = Database.connect(dataSource, databaseConfig = DatabaseConfig {
+        keepLoadedReferencesOutOfTransaction = true
+    })
+
+    val app = Javalin.create { config ->
+        config.jsonMapper(object : JsonMapper {
+            override fun <T : Any> fromJsonString(json: String, targetType: Type): T =
+                prettyGson.fromJson(json, targetType)
+
+            override fun toJsonString(obj: Any, type: Type) =
+                prettyGson.toJson(obj)
+        })
+
+        config.plugins.enableCors { cors ->
+            cors.add {
+                it.anyHost()
+            }
+        }
+
+        val routingPlugin = AnnotatedRoutingPlugin()
+        val gsonBuilder = gson.newBuilder()
+
+        transaction {
+            SchemaUtils.createMissingTablesAndColumns(ConfigurationTable)
+            Configuration
+
+            OAuth2ConnectionsManager.load(routingPlugin, gsonBuilder, config)
+            SessionManager
+
+            setOf(
+                AccountsTable,
+                AccountPasswordsTable,
+                OAuth2ConnectionsTable,
+                SessionsTable,
+            ).forEach(SchemaUtils::createMissingTablesAndColumns)
+        }
+
+        gson = gsonBuilder.create()
+        prettyGson = gsonBuilder
+            .setPrettyPrinting()
+            .create()
+
+        config.plugins.register(routingPlugin)
+    }.start(Env.PORT)
+}

backend/pom.xml
@@ -1,260 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<project xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xmlns="http://maven.apache.org/POM/4.0.0"
-         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-    <modelVersion>4.0.0</modelVersion>
-
-    <parent>
-        <groupId>sh.dominick.inpeel</groupId>
-        <artifactId>inpeel</artifactId>
-        <version>1.0.0</version>
-    </parent>
-
-    <packaging>jar</packaging>
-
-    <artifactId>backend</artifactId>
-    <name>backend</name>
-
-    <properties>
-        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
-        <kotlin.code.style>official</kotlin.code.style>
-        <kotlin.compiler.jvmTarget>1.8</kotlin.compiler.jvmTarget>
-    </properties>
-
-    <repositories>
-        <repository>
-            <id>mavenCentral</id>
-            <url>https://repo1.maven.org/maven2/</url>
-        </repository>
-    </repositories>
-
-    <build>
-        <sourceDirectory>src/main</sourceDirectory>
-        <testSourceDirectory>src/test</testSourceDirectory>
-        <finalName>app</finalName>
-        <plugins>
-            <plugin>
-                <groupId>org.jetbrains.kotlin</groupId>
-                <artifactId>kotlin-maven-plugin</artifactId>
-                <version>1.7.20</version>
-                <configuration>
-                    <javaParameters>true</javaParameters>
-                </configuration>
-                <executions>
-                    <execution>
-                        <id>compile</id>
-                        <phase>compile</phase>
-                        <goals>
-                            <goal>compile</goal>
-                        </goals>
-                    </execution>
-                    <execution>
-                        <id>test-compile</id>
-                        <phase>test-compile</phase>
-                        <goals>
-                            <goal>test-compile</goal>
-                        </goals>
-                    </execution>
-                </executions>
-            </plugin>
-            <plugin>
-                <artifactId>maven-surefire-plugin</artifactId>
-                <version>2.22.2</version>
-            </plugin>
-            <plugin>
-                <artifactId>maven-failsafe-plugin</artifactId>
-                <version>2.22.2</version>
-            </plugin>
-            <plugin>
-                <groupId>org.apache.maven.plugins</groupId>
-                <artifactId>maven-compiler-plugin</artifactId>
-                <configuration>
-                    <source>11</source>
-                    <target>11</target>
-                    <compilerArgs>
-                        <arg>-parameters</arg>
-                    </compilerArgs>
-                </configuration>
-            </plugin>
-            <plugin>
-                <groupId>org.apache.maven.plugins</groupId>
-                <artifactId>maven-jar-plugin</artifactId>
-                <version>2.6</version>
-                <configuration>
-                    <archive>
-                        <manifest>
-                            <addClasspath>true</addClasspath>
-                            <classpathPrefix>libs/</classpathPrefix>
-                            <mainClass>sh.dominick.inpeel.ApplicationKt</mainClass>
-                        </manifest>
-                    </archive>
-                </configuration>
-            </plugin>
-            <plugin>
-                <artifactId>maven-dependency-plugin</artifactId>
-                <executions>
-                    <execution>
-                        <phase>install</phase>
-                        <goals>
-                            <goal>copy-dependencies</goal>
-                        </goals>
-                        <configuration>
-                            <outputDirectory>${project.build.directory}/libs</outputDirectory>
-                        </configuration>
-                    </execution>
-                </executions>
-            </plugin>
-        </plugins>
-    </build>
-
-    <dependencies>
-        <dependency>
-            <groupId>org.jetbrains.kotlin</groupId>
-            <artifactId>kotlin-stdlib-jdk8</artifactId>
-            <version>1.7.20</version>
-        </dependency>
-
-        <!-- https://mvnrepository.com/artifact/io.javalin/javalin -->
-        <dependency>
-            <groupId>io.javalin</groupId>
-            <artifactId>javalin</artifactId>
-            <version>5.3.2</version>
-        </dependency>
-
-        <dependency>
-            <groupId>org.slf4j</groupId>
-            <artifactId>slf4j-simple</artifactId>
-            <version>2.0.3</version>
-        </dependency>
-
-        <dependency>
-            <groupId>org.jetbrains.exposed</groupId>
-            <artifactId>exposed-core</artifactId>
-            <version>0.40.1</version>
-        </dependency>
-        <dependency>
-            <groupId>org.jetbrains.exposed</groupId>
-            <artifactId>exposed-dao</artifactId>
-            <version>0.40.1</version>
-        </dependency>
-        <dependency>
-            <groupId>org.jetbrains.exposed</groupId>
-            <artifactId>exposed-jdbc</artifactId>
-            <version>0.40.1</version>
-        </dependency>
-
-        <dependency>
-            <groupId>redis.clients</groupId>
-            <artifactId>jedis</artifactId>
-            <version>4.3.0</version>
-        </dependency>
-
-        <dependency>
-            <groupId>io.github.cdimascio</groupId>
-            <artifactId>dotenv-kotlin</artifactId>
-            <version>6.4.1</version>
-        </dependency>
-
-        <dependency>
-            <groupId>io.jsonwebtoken</groupId>
-            <artifactId>jjwt-api</artifactId>
-            <version>0.11.5</version>
-        </dependency>
-        <dependency>
-            <groupId>io.jsonwebtoken</groupId>
-            <artifactId>jjwt-impl</artifactId>
-            <version>0.11.5</version>
-            <scope>runtime</scope>
-        </dependency>
-        <dependency>
-            <groupId>io.jsonwebtoken</groupId>
-            <artifactId>jjwt-jackson</artifactId>
-            <version>0.11.5</version>
-            <scope>runtime</scope>
-        </dependency>
-
-        <dependency>
-            <groupId>mysql</groupId>
-            <artifactId>mysql-connector-java</artifactId>
-            <version>8.0.32</version>
-        </dependency>
-
-        <dependency>
-            <groupId>com.zaxxer</groupId>
-            <artifactId>HikariCP</artifactId>
-            <version>4.0.3</version>
-        </dependency>
-
-        <dependency>
-            <groupId>io.javalin.community.routing</groupId>
-            <artifactId>routing-core</artifactId>
-            <version>5.5.0-RC.3</version>
-        </dependency>
-
-        <dependency>
-            <groupId>io.javalin.community.routing</groupId>
-            <artifactId>routing-annotated</artifactId>
-            <version>5.5.0-RC.3</version>
-        </dependency>
-
-        <dependency>
-            <groupId>io.javalin.community.routing</groupId>
-            <artifactId>routing-coroutines</artifactId>
-            <version>5.5.0-RC.3</version>
-        </dependency>
-
-        <!-- Argon2 Hashing Implementation -->
-        <!--  - Account Passwords -->
-        <dependency>
-            <groupId>de.mkammerer</groupId>
-            <artifactId>argon2-jvm</artifactId>
-            <version>2.7</version>
-        </dependency>
-
-        <!-- OAuth2 -->
-        <dependency>
-            <groupId>com.google.oauth-client</groupId>
-            <artifactId>google-oauth-client</artifactId>
-            <version>1.33.3</version>
-        </dependency>
-
-        <!-- Testing Dependencies -->
-        <dependency>
-            <groupId>junit</groupId>
-            <artifactId>junit</artifactId>
-            <version>4.13.1</version>
-            <scope>test</scope>
-        </dependency>
-
-        <dependency>
-            <groupId>org.assertj</groupId>
-            <artifactId>assertj-core</artifactId>
-            <version>3.11.1</version>
-            <scope>test</scope>
-        </dependency>
-
-        <dependency>
-            <groupId>io.mockk</groupId>
-            <artifactId>mockk</artifactId>
-            <version>1.12.5</version>
-            <scope>test</scope>
-        </dependency>
-
-        <dependency>
-            <groupId>org.xerial</groupId>
-            <artifactId>sqlite-jdbc</artifactId>
-            <version>3.42.0.0</version>
-            <scope>test</scope>
-        </dependency>
-
-        <dependency>
-            <groupId>com.h2database</groupId>
-            <artifactId>h2</artifactId>
-            <version>2.2.220</version>
-            <scope>test</scope>
-        </dependency>
-
-
-    </dependencies>
-
-</project>

backend/src/main/sh/dominick/inpeel/Application.kt
@@ -1,79 +1,0 @@
-package sh.dominick.inpeel
-
-import com.google.gson.GsonBuilder
-import com.google.gson.LongSerializationPolicy
-import com.zaxxer.hikari.HikariDataSource
-import io.javalin.Javalin
-import io.javalin.community.routing.annotations.AnnotatedRoutingPlugin
-import io.javalin.json.JsonMapper
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.DatabaseConfig
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.transactions.transaction
-import sh.dominick.inpeel.data.config.Configuration
-import sh.dominick.inpeel.data.config.ConfigurationTable
-import sh.dominick.inpeel.modules.accounts.AccountsModule
-import sh.dominick.inpeel.modules.sessions.SessionsModule
-import java.lang.reflect.Type
-
-var gson = GsonBuilder()
-    .disableHtmlEscaping()
-    .setLongSerializationPolicy(LongSerializationPolicy.STRING)
-    .create()
-private set
-
-var prettyGson = GsonBuilder().create()
-private set
-
-fun main() {
-    val dataSource = HikariDataSource().apply {
-        maximumPoolSize = Env.DATABASE_POOL_SIZE
-        driverClassName = Env.DATABASE_DRIVER
-        jdbcUrl = Env.DATABASE_URL
-        username = Env.DATABASE_USERNAME
-        password = Env.DATABASE_PASSWORD
-        isAutoCommit = false
-    }
-
-    val database = Database.connect(dataSource, databaseConfig = DatabaseConfig {
-        keepLoadedReferencesOutOfTransaction = true
-    })
-
-    val app = Javalin.create { config ->
-        config.jsonMapper(object : JsonMapper {
-            override fun <T : Any> fromJsonString(json: String, targetType: Type): T =
-                prettyGson.fromJson(json, targetType)
-
-            override fun toJsonString(obj: Any, type: Type) =
-                prettyGson.toJson(obj)
-        })
-
-        config.plugins.enableCors { cors ->
-            cors.add {
-                it.anyHost()
-            }
-        }
-
-        val routingPlugin = AnnotatedRoutingPlugin()
-        val gsonBuilder = gson.newBuilder()
-
-        transaction {
-            SchemaUtils.createMissingTablesAndColumns(ConfigurationTable)
-            Configuration
-        }
-
-        setOf(
-            AccountsModule,
-            SessionsModule
-        ).forEach { module ->
-            module.load(routingPlugin, gsonBuilder, config)
-        }
-
-        gson = gsonBuilder.create()
-        prettyGson = gsonBuilder
-            .setPrettyPrinting()
-            .create()
-
-        config.plugins.register(routingPlugin)
-    }.start(Env.PORT)
-}

pom.xml
@@ -13,6 +13,7 @@
     <name>inpeel</name>
 
     <modules>
-        <module>backend</module>
+        <module>rest-backend</module>
+        <module>lib</module>
     </modules>
 </project>

commit 4ba8015dfd72d01e914a7c11b78eabe1858b68f1

Create manager to register PasswordSessionTransformer (regression from 9d6094bed8999fbc9e964c75ce5f5fbf258c0c06)

lib/pom.xml
@@ -33,14 +33,6 @@
                 <groupId>org.apache.maven.plugins</groupId>
                 <artifactId>maven-resources-plugin</artifactId>
                 <version>3.3.1</version>
-
-                <configuration>
-                    <testResources>
-                        <testResource>
-                            <directory>${project.basedir}/src/test</directory>
-                        </testResource>
-                    </testResources>
-                </configuration>
             </plugin>
             <plugin>
                 <groupId>org.jetbrains.kotlin</groupId>

lib/src/main/sh/dominick/inpeel/lib/managers/PasswordsManager.kt
@@ -1,0 +1,18 @@
+package sh.dominick.inpeel.lib.managers
+
+import com.google.gson.GsonBuilder
+import io.javalin.community.routing.annotations.AnnotatedRoutingPlugin
+import io.javalin.config.JavalinConfig
+import sh.dominick.inpeel.lib.ApplicationInitialized
+import sh.dominick.inpeel.lib.data.passwords.BasicAuth
+import sh.dominick.inpeel.lib.data.passwords.PasswordSessionTransformer
+
+object PasswordsManager : ApplicationInitialized {
+    override fun load(routingPlugin: AnnotatedRoutingPlugin, gsonBuilder: GsonBuilder, javalinConfig: JavalinConfig) {
+        AuthorizationManager.registerType("Basic") {
+            BasicAuth.match(it)
+        }
+
+        SessionManager.registerType("password", PasswordSessionTransformer)
+    }
+}

rest-backend/src/main/sh/dominick/inpeel/rest/Application.kt
@@ -17,6 +17,7 @@ import sh.dominick.inpeel.lib.data.sql.AccountsTable
 import sh.dominick.inpeel.lib.data.sql.Configuration
 import sh.dominick.inpeel.lib.data.sql.ConfigurationTable
 import sh.dominick.inpeel.lib.managers.OAuth2ConnectionsManager
+import sh.dominick.inpeel.lib.managers.PasswordsManager
 import sh.dominick.inpeel.lib.managers.SessionManager
 import java.lang.reflect.Type
 
@@ -65,8 +66,9 @@ fun main() {
             SchemaUtils.createMissingTablesAndColumns(ConfigurationTable)
             Configuration
 
-            OAuth2ConnectionsManager.load(routingPlugin, gsonBuilder, config)
             SessionManager
+            OAuth2ConnectionsManager.load(routingPlugin, gsonBuilder, config)
+            PasswordsManager.load(routingPlugin, gsonBuilder, config)
 
             setOf(
                 AccountsTable,